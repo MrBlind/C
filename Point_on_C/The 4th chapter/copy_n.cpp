99e68cafd33835aa6d59d398ba1ae7cb948c3a58
MrBlind <2984067240@qq.com>
2017-02-16 14:00:23 +0800

    /*   ** Extract the specified substring from the string in src.   */

---
+++ Point_on_C/The 4th chapter/copy_n.cpp
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_DEPRECATE
+#include<stdio.h>
+
+void copy_n(char dst[], char src[], int n)
+{
+	int src_n;
+	int dst_n;
+	src_n = 0;
+	for (dst_n = 0; dst_n < n; dst_n++)
+	{
+		dst[dst_n] = src[src_n];
+		if (src[src_n] != 0)
+		{
+			src_n++;
+		}
+	}
+	dst[dst_n] = '\0';
+}
+
+
+int main()
+{
+	char dst[20];
+	char src[20];
+	int n;
+	scanf("%d", &n);
+	scanf("%s", src);
+	copy_n(dst, src, n);
+	printf("%s", dst);
+	return 0;
+}
+
+
