0f86ce4d4214782de38f2aecbf534c2f19eef296
MrBlind <2984067240@qq.com>
2018-12-30 16:38:37 +0800

    add Introduction to Algorithms

---
+++ Introduction_to_Algotithms/include/INSERTION_SORT.h
@@ -0,0 +1,14 @@
+#ifndef __insertion_sort_h
+#define __insertion_sort_h
+
+#include <stdio.h>
+
+typedef struct _Array {
+	int length;
+	int arr[100];
+}Array;
+
+int insertion_sort(Array * test);
+
+#endif
+
